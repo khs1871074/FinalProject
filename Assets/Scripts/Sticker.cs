a35acd1186af99a820e788a897047cabf6dded82
khs1871074 <khs1871074@gmail.com>
2023-06-16 11:40:51 +0900

    기초 메소드 추가
    구현 예정

---
+++ Assets/Scripts/Sticker.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum IsType
+{
+    Pawn,
+    Rook,
+    Bishop,
+    Knight,
+    Queen,
+    King,
+}
+
+public class Sticker : MonoBehaviour
+{
+    protected bool isAttach;
+    protected Vector2Int[] movement;
+    
+    public IsType isType;
+
+    protected void OnMouseDown()
+    {
+        
+    }
+
+    protected void OnMouseDrag()
+    {
+        
+    }
+
+    protected void OnMouseUp()
+    {
+        
+    }
+    
+}
