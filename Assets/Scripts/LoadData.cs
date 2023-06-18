d2886f44ac0318b2cce729bbcf0c1beffb450d81
khs1871074 <khs1871074@gmail.com>
2023-06-18 20:05:07 +0900

    Stage 추가, 최종 클리어 화면 추가
    Stage4, Stage5, Stage6 추가

--- Assets/Scripts/LoadData.cs
+++ Assets/Scripts/LoadData.cs
@@ -7,14 +7,24 @@ public class LoadData : MonoBehaviour
 {
     private SaveData saveData;
     private bool[] stageClearList;
+    private int clearCount = 0;
     
     public Button[] stageButton;
+    public GameObject allClear;
+    public GameObject allClearParticle;
+    public Transform allClearParticlePos;
     // Start is called before the first frame update
     void Start()
     {
+        allClear.SetActive(false);
         saveData = GameObject.Find("SaveData").GetComponent<SaveData>();
         stageClearList = saveData.GetClearList();
         SetClearStage();
+        if (clearCount == stageClearList.Length)
+        {
+            Instantiate(allClearParticle, allClearParticlePos.position, allClearParticlePos.rotation);
+            allClear.SetActive(true);
+        }
     }
 
     private void SetClearStage()
@@ -23,6 +33,7 @@ public class LoadData : MonoBehaviour
         {
             if (stageClearList[i])
             {
+                clearCount++;
                 stageButton[i].interactable = false;
             }
         }
